a4d0ae65595ac4534e554be7383899737468f994
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-25 21:43:02 +0000

    updated db connection for railway

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // For local development, use the DATABASE_URL from .env
-  // connectionString: process.env.DATABASE_URL,
-  // For production, use the connection string from environment variable
-  connectionString: Postgres.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
